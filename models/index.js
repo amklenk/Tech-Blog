20117f32617e6bc9f7d3498f23b8b64f8681a8ca
Amanda Klenk <amandamklenk3@gmail.com>
2022-07-09 15:25:26 -0400

    set up associations between User and Post in index.js in models folder

--- models/index.js
+++ models/index.js
@@ -1,5 +1,18 @@
+//require
 const User = require('./User');
 const Post = require('./Post');
+
+//associations
+//joins User and Post
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+//
 
 
 //exports
