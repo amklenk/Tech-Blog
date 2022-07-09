1535376ba14834451c7a11f4c91dbe684283d084
Amanda Klenk <amandamklenk3@gmail.com>
2022-07-09 10:44:44 -0400

    set up user model

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+//require
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+//connection to db using .env variables
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
+
+//export
+module.exports = sequelize;
---
+++ models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+
+
+
+//exports
+module.exports = { User }
