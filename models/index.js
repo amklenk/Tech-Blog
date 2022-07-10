ac32a306b90a63c643da0cf12c35a2b18cac94f2
Amanda Klenk <amandamklenk3@gmail.com>
2022-07-10 14:18:44 -0400

    created associations for joining Comment with User and Post in index.js in models directory

--- models/index.js
+++ models/index.js
@@ -13,7 +13,22 @@ Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-//
+//joins Comment with User and Post
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
 
 
 //exports
