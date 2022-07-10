ce1034a18920c02aeb802f6a79790f4c31b39803
Amanda Klenk <amandamklenk3@gmail.com>
2022-07-10 13:51:34 -0400

    set up seed file for comments, will continue with comments feature branch

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,11 @@
+//require
+const { Comment } = require('../models');
+
+const commentData= [];
+
+const seedComments = () => Comment.bulkCreate(commentData);
+
+module.exports = seedComments;
+
+
+
