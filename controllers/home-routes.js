8f5219c2be219f1b8cd8d947334c08969b686257
Amanda Klenk <amandamklenk3@gmail.com>
2022-07-11 16:45:51 -0400

    looped over posts to render posts on homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,16 +1,35 @@
+//require
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-  res.render('homepage', {
-    id: 1,
-    title: "Salt and Hash Your Passwords!",
-	text: "It is important to protect your users' data. You can do this with salting and hashing their passwords. This will protect their password from being hacked from your database. You can use bcrypt for this!",
-    title: 'Handlebars Docs',
-    created_at: new Date(),
-    comments: [{}, {}],
-    user: {
-      username: 'test_user'
-  }});
+    Post.findAll({
+        attributes: ["id", "title", "text", "created_at"],
+        order: [["created_at", "DESC"]],
+        include: [
+          {
+            model: Comment,
+            attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+            include: {
+              model: User,
+              attributes: ["username"],
+            },
+          },
+          {
+            model: User,
+            attributes: ["username"],
+          },
+        ],
+      })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('homepage', { posts });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
 });
 
 module.exports = router;
