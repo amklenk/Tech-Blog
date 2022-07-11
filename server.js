44bce45eaa5a5324c4f37e9553412b37f6cda404
Amanda Klenk <amandamklenk3@gmail.com>
2022-07-11 13:21:07 -0400

    initial path and public setup

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 //require
+const path = require('path');
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3001;
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //turns on routes
 app.use(routes);
