9ca0f44e62931351e028d22bf56a5dd7ca61766b
Amanda Klenk <amandamklenk3@gmail.com>
2022-07-11 20:14:40 -0400

    installed and set up sessions in server and altered routes for logging in

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,6 +4,7 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
+    console.log(req.session);
     Post.findAll({
         attributes: ["id", "title", "text", "created_at"],
         order: [["created_at", "DESC"]],
@@ -33,6 +34,10 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
     res.render('login');
   });
 
