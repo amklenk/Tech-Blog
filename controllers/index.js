6256ff52e11a58e2b404143b4f125cbbaea41e98
Amanda Klenk <amandamklenk3@gmail.com>
2022-07-12 16:23:59 -0400

    created dashboard with create functionality, updated post-routes for POST

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 //require
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js')
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 //check to make sure endpoint exists
 router.use((req, res) => {
