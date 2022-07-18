436f79b38888107c8820df520ecbcf0b160942dd
Amanda Klenk <amandamklenk3@gmail.com>
2022-07-18 12:13:22 -0400

    added timeout functionality in cookie of sess

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: 'Super secret secret',
-    cookie: {},
+    cookie: {  maxAge: 60000 },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
       db: sequelize
-    })
+    }),
   };
 
 app.use(session(sess));
